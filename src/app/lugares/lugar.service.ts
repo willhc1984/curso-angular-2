1aff5115e69f2667535e713b466190c3f99c1538
William Henrique Campos <william.campos@camarasorocaba.sp.gov.br>
2026-07-15 15:17:23 -0300

    POST de lugar com API Json Server

---
+++ src/app/lugares/lugar.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Lugar } from './lugar';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LugarService {
+
+  constructor(private http: HttpClient) { }
+
+  salvar(lugar: Lugar) : Observable<Lugar>{
+    return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+  }
+
+  obterTodos() : Observable<Lugar[]>{
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+
+}
---
+++ src/app/lugares/lugar/lugar.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
+
+@Component({
+  selector: 'app-lugar',
+  standalone: false,
+  templateUrl: './lugar.component.html',
+  styleUrl: './lugar.component.scss'
+})
+
+export class LugarComponent implements OnInit {
+  camposForm : FormGroup;
+  categorias : Categoria[] = [];
+
+  constructor(private categoriaService : CategoriaService, private lugarService: LugarService){
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required)
+    });
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.listarTodas().subscribe({
+      next: (listaCategorias) => {
+        this.categorias = listaCategorias
+      }
+    })
+  }
+
+  salvar(){
+    this.lugarService.salvar(this.camposForm.value)
+      .subscribe({
+        next: (lugar) => {
+          console.log('cadastrado com sucesso: ', lugar);
+          this.camposForm.reset();
+        },
+        error: erro => console.log('Ocorreu um erro: ', erro)
+      })
+  }
+
+
+}
