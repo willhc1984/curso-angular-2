0312ba6de54c375e86f07e3a84476679ecd2f47a
William Henrique Campos <william.campos@camarasorocaba.sp.gov.br>
2026-07-27 19:50:46 -0300

    AuthService para realizar login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertaService } from '../../alerta.service';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +10,35 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
+
+  camposForm: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router, private alerta: AlertaService){
+    this.camposForm = new FormGroup({
+      email: new FormControl('', Validators.email),
+      senha: new FormControl('', Validators.required)
+    })
+  }
+
+  login() : void {
+    this.authService.login(
+      this.camposForm.value.email,
+      this.camposForm.value.senha
+    ).subscribe({
+      next: usuarios => {
+        if(usuarios.length > 0){
+          localStorage.setItem(
+            'usuario', 
+            JSON.stringify(usuarios[0]),
+          );
+          this.router.navigate(['/paginas/categorias/consulta'])
+        }else{
+          this.alerta.erro('Usuário ou senha inválidos!');
+        }
+      }
+    })
+  }
 
 }
